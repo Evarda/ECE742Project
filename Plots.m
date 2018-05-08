67ff739894af3ca43f95455892bf92c5479ec427
surisuraj <surisuraj@gmail.com>
2018-05-08 16:34:08 -0500

    Added new plots

--- Plots.m
+++ Plots.m
@@ -1,13 +1,13 @@
-figure
-semilogy((1:nmax)*dt,abs(Error(2:6,:)-Error(1,:))/abs(max(Error(1,:))));
-legend('\sigma_{max}=10^{-1}','\sigma_{max}=10^{0}','\sigma_{max}=10^{1}','\sigma_{max}=10^{2}','\sigma_{max}=10^{3}','location','southeast');
-hold off
-grid minor
-grid on
-title('Relative Error over Time for Varying Values of \sigma_{max}, m = 2, \kappa_{max} = 3')
-xlabel('Time (s)');
-ylabel('Relative Error')
-axis([0, 2.1e-9, 1e-10, 10])
+% figure
+% semilogy((1:nmax)*dt,abs(Error(2:6,:)-Error(1,:))/abs(max(Error(1,:))));
+% legend('\sigma_{max}=10^{-1}','\sigma_{max}=10^{0}','\sigma_{max}=10^{1}','\sigma_{max}=10^{2}','\sigma_{max}=10^{3}','location','southeast');
+% hold off
+% grid minor
+% grid on
+% title('Relative Error over Time for Varying Values of \sigma_{max}, m = 2, \kappa_{max} = 3')
+% xlabel('Time (s)');
+% ylabel('Relative Error')
+% axis([0, 2.1e-9, 1e-10, 10])
 % 
 % figure
 % semilogy((1:nmax)*dt,abs(Error(7:12,:)-Error(1,:))/abs(max(Error(1,:))));
